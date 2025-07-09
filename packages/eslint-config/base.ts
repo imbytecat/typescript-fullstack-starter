e828daca6410ede97300c31e776ce196fb897b6f
imbytecat <imbytecat@gmail.com>
2025-07-09 21:56:15 +0800

    chore: 增加和修改ESLint规则
    - 增加和修改ESLint规则以增强代码质量控制。

--- packages/eslint-config/base.ts
+++ packages/eslint-config/base.ts
@@ -9,4 +9,15 @@ export const baseConfig = antfu({
     '**/node_modules',
     '**/dist',
   ],
+}, {
+  rules: {
+    'antfu/if-newline': 'off',
+    'eslint-comments/no-unlimited-disable': ['off'],
+    'no-console': ['warn'],
+    'perfectionist/sort-imports': ['error', { tsconfigRootDir: '.' }],
+    'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+    'style/max-statements-per-line': ['error', { max: 2 }],
+    'ts/consistent-type-definitions': ['off'],
+    'ts/no-empty-object-type': ['off'],
+  },
 })

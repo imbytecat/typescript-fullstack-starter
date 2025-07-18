18f30cb1fb735ce673b1c02b989d7a3d984a93f8
imbytecat <imbytecat@gmail.com>
2025-07-18 19:36:29 +0800

    feat: 添加POST接口并进行zod数据验证
    - 新增通过POST请求和zod验证用户输入数据的接口功能。

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,27 @@
+import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
+import z from 'zod'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  .get(
+    '/',
+    (c) => {
+      return c.text('Hello Hono!')
+    },
+  )
+
+  .post(
+    '/',
+    zValidator('json', z.object({
+      name: z.string(),
+    })),
+    (c) => {
+      const { name } = c.req.valid('json')
+      return c.json({
+        message: `Hello ${name}!`,
+      })
+    },
+  )
 
 export default app
